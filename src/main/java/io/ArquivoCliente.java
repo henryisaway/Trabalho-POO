a54a2eef6956cf8ceaf70b92f5c3009e139545c8
pilanza <pietrolp333@gmail.com>
2025-08-05 08:59:55 -0300

    refactor & feat: arrumei a estrutura de pastas uma última vez, e atualizei o código de arquivo cliente

--- src/main/java/io/ArquivoCliente.java
+++ src/main/java/io/ArquivoCliente.java
@@ -1,6 +1,7 @@
 package io;
 
 import java.io.*;
+import java.time.LocalDate;
 import java.util.*;
 
 public class ArquivoCliente extends Arquivo {
@@ -11,13 +12,30 @@ public class ArquivoCliente extends Arquivo {
     public void criaClientes() throws ArquivoException {
         String conteudoArquivo = super.getTextoArquivo();
         String[] linhas = conteudoArquivo.split("\\r?\\n");
-        for(String linha:linhas) {
-            System.out.println(linha);
+        String[] linha;
+        int codigo, numEscricao;
+        String nome, endereco, telefone, cpf, cnpj, tipoCliente;
+        LocalDate dataCadastro;
+
+        for(int i=1; i < linhas.length; i++) {
+            linha = linhas[i].split(";");
+            codigo = Integer.parseInt(linha[0]);
+            nome = linha[1];
+            endereco = linha[2];
+            telefone = linha[3];
+            dataCadastro = LocalDate.parse(linha[4]);
+            tipoCliente = linha[5];
+            numEscricao = Integer.parseInt(linha[7]);
+            if(tipoCliente.equals("F")) {
+                cpf = linha[6];
+            } else {
+                cnpj = linha[6];
+            }
         }
     }
 
     public static void main(String[] args) throws ArquivoException {
-        ArquivoCliente clientes = new ArquivoCliente(new File("/home/pietro/Documents/github/Trabalho-POO/src/resources/cadastro_clientes.csv"));
+        ArquivoCliente clientes = new ArquivoCliente(new File("/home/pilanza/Documents/github/Trabalho-POO/src/resources/cadastro_clientes.csv"));
         clientes.criaClientes();
     }
 }
